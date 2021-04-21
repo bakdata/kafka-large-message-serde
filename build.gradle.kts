0594d660c4323c71f4c60bf9a2898f4bfc29d4c6
Philipp Schirmer <philipp.schirmer@bakdata.com>
2021-04-21 17:42:57 +0200

    Update gradle plugins (#11)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("net.researchgate.release") version "2.8.0"
-    id("com.bakdata.sonar") version "1.1.4"
-    id("com.bakdata.sonatype") version "1.1.4"
+    id("com.bakdata.sonar") version "1.1.6"
+    id("com.bakdata.sonatype") version "1.1.6"
     id("org.hildan.github.changelog") version "0.8.0"
     id("io.freefair.lombok") version "3.8.0"
 }
