ba632be185c702441017488d76b4fd8a0f3bf3d2
Jan Doms <jan.doms@klarrio.com>
2022-09-12 09:36:58 +0200

    add header, make property private

--- large-message-core/src/main/java/com/bakdata/kafka/CompressionType.java
+++ large-message-core/src/main/java/com/bakdata/kafka/CompressionType.java
@@ -1,4 +1,28 @@
-package com.bakdata.kafka;
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 bakdata
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+ package com.bakdata.kafka;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,6 +39,9 @@ import org.apache.kafka.common.utils.ByteBufferOutputStream;
 
 import software.amazon.awssdk.utils.IoUtils;
 
+/**
+ * This enum specifies the various allowed compression types and their implementation.
+ */
 public enum CompressionType {
     NONE(0, "none") {
         @Override
@@ -93,7 +120,8 @@ public enum CompressionType {
     @Getter
     private final byte id;
     @NonNull
-    public final String name;
+    @Getter
+    private final String name;
 
     CompressionType(int id, String name) {
         this.id = (byte)id;
