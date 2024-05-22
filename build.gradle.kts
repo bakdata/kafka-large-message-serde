ceeac2032b19114522ef9aee3194f44c45166d62
Torben Meyer <torben.meyer@bakdata.com>
2024-05-22 12:28:54 +0200

    Update plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-    id("net.researchgate.release") version "3.0.2"
-    id("com.bakdata.sonar") version "1.1.17"
-    id("com.bakdata.sonatype") version "1.1.14"
-    id("org.hildan.github.changelog") version "2.2.0"
+    id("com.bakdata.release") version "1.4.0"
+    id("com.bakdata.sonar") version "1.4.0"
+    id("com.bakdata.sonatype") version "1.4.0"
     id("io.freefair.lombok") version "8.4"
 }
 
