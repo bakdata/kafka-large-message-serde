19cd64085477a5f61d5ca1dbb31c9b9cb054c011
Yidian Sun <yidian.sun@enterprisedb.com>
2024-12-27 21:21:39 +0800

    format

--- large-message-core/src/main/java/com/bakdata/kafka/AbstractLargeMessageConfig.java
+++ large-message-core/src/main/java/com/bakdata/kafka/AbstractLargeMessageConfig.java
@@ -45,7 +45,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Supplier;
-
 import io.confluent.common.utils.Utils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.errors.SerializationException;
