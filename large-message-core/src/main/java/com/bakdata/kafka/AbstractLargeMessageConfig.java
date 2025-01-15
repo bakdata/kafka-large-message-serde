6c64b306e0268b52ac3a40c101f9d80906f9b00a
Yidian Sun <yidian.sun@enterprisedb.com>
2025-01-15 18:20:39 +0800

    format

--- large-message-core/src/main/java/com/bakdata/kafka/AbstractLargeMessageConfig.java
+++ large-message-core/src/main/java/com/bakdata/kafka/AbstractLargeMessageConfig.java
@@ -40,7 +40,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Supplier;
-
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.config.AbstractConfig;
