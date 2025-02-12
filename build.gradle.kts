f76fe05ff60908b340cdb1b20c3b493e5a4b36f1
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-02-12 09:17:48 +0100

    Upgrade to AWS SDK 2.30

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.bakdata.release") version "1.6.1"
-    id("com.bakdata.sonar") version "1.6.1"
-    id("com.bakdata.sonatype") version "1.6.1"
+    id("com.bakdata.release") version "1.7.1"
+    id("com.bakdata.sonar") version "1.7.1"
+    id("com.bakdata.sonatype") version "1.7.1"
     id("io.freefair.lombok") version "8.11"
 }
 
