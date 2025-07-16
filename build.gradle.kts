d6237fc1913f653b8de65699f9f6011f892c3977
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-07-16 17:52:10 +0200

    Upgrade to Kafka 4.0 and Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ allprojects {
 
     repositories {
         mavenCentral()
-        maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
+        maven(url = "https://central.sonatype.com/repository/maven-snapshots")
     }
 }
 
